2f8b8700f126fa05a2a06848da506d4e53b13250
Louis Kane <louiskane@1234.local>
2017-01-29 21:03:16 -0800

    added html routes for all pages, a new login page and new styling for landing page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,8 +2,43 @@ var path = require ("path");
 
 module.exports = function(app){
 
-	
+	//home page
 	app.get("/", function(req, res){
-		res.sendFile(path.join(__dirname + "/../public/view.html"))
+		res.sendFile(path.join(__dirname + "/../public/landing.html"))
 	})
+
+	//found page
+	app.get("/found", function(req, res){
+		res.sendFile(path.join(__dirname + "/../public/foundList.html"))
+	})
+
+	//lost page
+	app.get("/lost", function(req, res){
+		res.sendFile(path.join(__dirname + "/../public/lostList.html"))
+	})
+
+	//new post page
+	app.get("/post", function(req, res){
+		res.sendFile(path.join(__dirname + "/../public/foundLostForm.html"))
+	})
+
+	//login page
+	app.get("/login", function(req, res){
+		res.sendFile(path.join(__dirname + "/../public/login.html"))
+	})
+
+	//create new account page
+	app.get("/newaccount", function(req, res){
+		res.sendFile(path.join(__dirname + "/../public/newAccountPage.html"))
+	})
+
+	//acount page
+	app.get("/account", function(req, res){
+		res.sendFile(path.join(__dirname + "/../public/accountPage.html"))
+	})
+
+	// If no matching route is found default to home
+	app.use(function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/landing.html'));
+	});
 }
