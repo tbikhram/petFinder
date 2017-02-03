95c8ea256a4d7c8b80234a2c33ed8e67ca82b671
arusmanukyan <manukyan.arus@gmail.com>
2017-02-02 17:30:41 -0800

    model and api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,52 +1,44 @@
 var db = require ("../models");
 
-//route keys:
-//findAll
-//findOne
-//create
-//update
-
-//petFinder = user
 module.exports = function(app){
 
 	//GET route to get all users
-	app.get("petFinder", function(req, res){
-		db.petFinder.findAll({}).then(function(dbpetFinder){
-			res.json(dbpetFinder);
+	app.get("/api/petFinder", function(req, res){
+		db.Author.findAll({}).then(function(dbpetFinder){
+			res.json(dbAuthor);
 		});
 	});
 
 	//GET route to get single user
-	app.get("petFinder/:id", function(req, res){
-		db.petFinder.findOne({
+	app.get("/api/petFinder/:id", function(req, res){
+		db.Author.findOne({
 			where: {
 				id: req.params.id
 			}
-		}).then(function(dbpetFinder){
-			res.json(dbpetFinder);
+		}).then(function(dbAuthor){
+			res.json(dbAuthor);
 		});
 	});
 
 	//POST route for saving a new user
-	app.post("petFinder", function(req, res){
-		db.petFinder.create(req.body).then(function(dbpetFinder){
-			res.json(dbpetFinder);
+	app.post("/api/petFinder", function(req, res){
+		db.Author.create(req.body).then(function(dbAuthor){
+			res.json(dbAuthor);
 		});
 	});
 
 	//PUT route for updating a new user
 
-	app.put("petFinder", function(req, res) {
+	app.put("/api/petFinder", function(req, res) {
 		db.petFinder.update(
 			req.body,
 			{
 				where: {
 					id: req.body.id
 				}
-			}).then(function(dbpetFinder) {
-				res.json(dbpetFinder);
+			}).then(function(dbAuthor) {
+				res.json(dbAuthor);
 			});
 		});
-//END OF MODULE.EXPORTS
 
 };
